f9d1632e99d0b9e22056faa0f537d887358f49d0
Priyanshu Gupta <priyanshug1999@gmail.com>
2023-04-28 01:36:45 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,51 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import './index.css'; //for tailwind css
 import App from './App';
+import ErrorPage from './components/ErrorPage';
 import reportWebVitals from './reportWebVitals';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Main from './components/student/Main';
+import Test from './components/Test';
+import SignUp from './components/SignUp';
+import Login from './components/Login';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "signUp",
+        element: <SignUp />,
+      },
+      {
+        path: "login",
+        element: <Login />,
+      },
+    ],
+  },
+  {
+    path :"main",
+    element:<Main/>
+  },
+  {
+    path :"testApi",
+    element:<Test/>
+  }
+]);
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  // <React.StrictMode>
+    <RouterProvider router={router} />
+  /* </React.StrictMode> */
 );
 
 // If you want to start measuring performance in your app, pass a function
