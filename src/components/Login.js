f0ec8069df55d0de48aaedc89235b41077006645
Priyanshu Gupta <priyanshug1999@gmail.com>
2023-05-06 00:23:01 +0530

    Update Login.js

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,19 +1,19 @@
-import { useState ,useEffect,useRef} from "react";
+import { useState, useEffect, useRef } from "react";
 import constants from "../utility/constants";
 import axios from "axios";
 import { Navigate } from "react-router-dom";
 function Login() {
 
     useEffect(() => {
-        let token=localStorage.getItem("token")
-        if(token!=null && token.length>0){
+        let token = localStorage.getItem("token")
+        if (token != null && token.length > 0) {
             setNavigate(true);
-        }else{
+        } else {
             setNavigate(false);
         }
     }, [])
 
-    
+
     const MRef = useRef(null);
     MRef.current = window.M;
 
@@ -23,7 +23,6 @@ function Login() {
 
     async function submitForm(event) {
         event.preventDefault();
-        console.log(enrollment, password);
         let data = {
             "enrollmentNo": enrollment,
             "password": password
@@ -33,51 +32,61 @@ function Login() {
             localStorage.setItem("token", response.data.token);
             setNavigate(true);
         } catch (error) {
-            if (error.response.status === 404) {
-                // handle 404 error
-                console.log(error.response.data.message);
-                MRef.current.toast({html:error.response.data.message, classes: 'rounded',displayLength:10000});
+            console.log("error", error);
+            if (error.response) {
+                if (error.response.status === 404) {
+                    // handle 404 error
+                    console.log(error.response.data.message);
+                    MRef.current.toast({ html: error.response.data.message, classes: 'rounded bg-1', displayLength: 5000 });
+                } else {
+                    // handle other errors
+                    console.log(error.response.data.message);
+                    MRef.current.toast({ html: error.response.data.message, classes: 'rounded bg-1', displayLength: 5000 });
+                }
             } else {
-                // handle other errors
-                console.log(error.message);
-                MRef.current.toast({html:error.response.data.message, classes: 'rounded',displayLength:10000});
+                MRef.current.toast({ html: error.message, classes: 'rounded bg-1', displayLength: 50000 });
             }
         }
     }
 
     if (navigate) {
-        return <Navigate to="/main" replace={true} />;
+        return <Navigate to="/student" replace={true} />;
     }
 
 
     return (
         <>
-            <section className="container">
-                <p className="center-align fs-1 fw-normal txt-col1">Log In</p>
+            <section className="container login-card">
                 <div className="row">
-                    <form className="col s12 m6 offset-m3" onSubmit={submitForm}>
-
-                        <div className="row">
-                            <div className="input-field col s12">
-                                <i className="material-icons prefix">assignment_ind</i>
-                                <input id="enrollment" type="text" className="validate" required onChange={(event) => setEnrollment(event.target.value)} />
-                                <label htmlFor="enrollment">Enrollment No.</label>
+                    <div className="col s12 m6 offset-m3">
+                        <form onSubmit={submitForm}>
+                            <div className="card">
+                                <div className="card-content">
+                                    <span className="card-title center align txt-col1">Login</span>
+                                    <div className="input-field">
+                                        <i className="material-icons prefix">assignment_ind</i>
+                                        <input id="enrollment" type="text" className="validate" required onChange={(event) => setEnrollment(event.target.value)} />
+                                        <label htmlFor="enrollment">Enrollment No.</label>
+                                    </div>
+                                    <div className="input-field">
+                                        <i className="material-icons prefix">vpn_key</i>
+                                        <input id="password" type="password" className="validate" required onChange={(event) => setPassword(event.target.value)} />
+                                        <label htmlFor="password">Password</label>
+                                    </div>
+                                    <a href="#!" id="forget-pass">
+                                        Forgot Password
+                                    </a>
+                                </div>
+                                <div className="card-action center-align">
+                                    <button className="btn waves-effect waves-light btn-1" type="submit" >Submit
+                                        <i className="material-icons right">send</i>
+                                    </button>
+                                </div>
                             </div>
-                            <div className="input-field col s12">
-                                <i className="material-icons prefix">vpn_key</i>
-                                <input id="password" type="password" className="validate" required onChange={(event) => setPassword(event.target.value)} />
-                                <label htmlFor="password">Password</label>
-                            </div>
-                        </div>
-                        <div className="center-align">
-                            <button className="btn waves-effect waves-light btn-1" type="submit" >Submit
-                                <i className="material-icons right">send</i>
-                            </button>
-                        </div>
-                    </form>
+                        </form>
+                    </div>
                 </div>
             </section>
-
         </>
     );
 }
