d05655b88f874f8c0a68c1e765a8b450d23b5837
Priyanshu Gupta <priyanshug1999@gmail.com>
2023-05-21 21:32:10 +0530

    Create ProtectedRoute.js

---
+++ src/components/student/ProtectedRoute.js
@@ -0,0 +1,49 @@
+import { useEffect, useState } from "react";
+import axios from 'axios';
+import constants from "../../utility/constants";
+import { Navigate} from 'react-router-dom';
+
+function ProtectedRoute(props) {
+    const [userDetails, setUserDetails] = useState({});
+    const [redirectToSignIn, setRedirectToSignIn] = useState(false);
+    const ChildComponent=props.Component;
+
+    async function fetchData(token) {
+        console.log(`Bearer ${token}`);
+        await axios.get(constants.API_URL + "/api/user", {
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        })
+        .then((response) => {
+            console.log(response);
+            setUserDetails(response.data);
+        })
+        .catch((error) => {
+            localStorage.removeItem('token');
+            console.log(error);
+            setRedirectToSignIn(true);
+        });
+    }
+
+    useEffect(() => {
+        const token = localStorage.getItem("token");
+        if (token != null) {
+            fetchData(token);
+        } else {
+            setRedirectToSignIn(true);
+        }
+    }, []);
+
+    if (redirectToSignIn) {
+        return <Navigate to="/login" replace={true} />;
+    }
+
+    return (
+        <>
+        <ChildComponent data={userDetails}/>
+        </>
+    );
+}
+
+export default ProtectedRoute;
